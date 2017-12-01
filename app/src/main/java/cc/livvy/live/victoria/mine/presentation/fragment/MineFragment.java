3fd9eed2d4502353761ae140adc08769f8e568b9
livvy <lvm@kingnet.com>
2017-12-01 10:10:16 +0800

    init

---
+++ app/src/main/java/cc/livvy/live/victoria/mine/presentation/fragment/MineFragment.java
@@ -0,0 +1,44 @@
+package cc.livvy.live.victoria.mine.presentation.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by livvy on 17-12-1.
+ */
+
+public class MineFragment extends BaseFragment {
+
+    private View rootView;
+    private boolean isFirst = true;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+        } else {
+            isFirst = false;
+        }
+        ViewGroup parent = (ViewGroup) rootView.getParent();
+        if (parent != null) {
+            parent.removeView(rootView);
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (isFirst) {
+            initView(view);
+        }
+    }
+
+    private void initView(View view) {
+
+    }
+}
